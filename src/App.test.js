09e7a5c50285770bd474d0e8acb4a83d466982f6
Dennis Kristiansen <denniskristiansen@protonmail.com>
2022-01-28 19:24:00 +0100

    Setup testing for react components

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom"
 import React from "react"
+import "@testing-library/jest-dom"
+import { render } from "@testing-library/react"
 import { Inspector } from "./App"
+
+/*
+ * See: https://create-react-app.dev/docs/running-tests
+ */
 
 describe("The Inspector component", () => {
 	it("renders without throwing", () => {
@@ -12,5 +18,15 @@ describe("The Inspector component", () => {
 		// Render the component and fail if it throws
 		const root = document.createElement("div")
 		ReactDOM.render(<Inspector selected={dummySelectedData} />, root)
+	})
+
+	it("renders the supplied properties", () => {
+		// Random dummy data just to satisfy the component
+		const dummySelectedData = {
+			properties: ["First", "Second"],
+		}
+
+		const comp = render(<Inspector selected={dummySelectedData} />)
+		expect(comp.getByText("First")).toBeInTheDocument()
 	})
 })
