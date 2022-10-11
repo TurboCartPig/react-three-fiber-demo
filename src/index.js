32917c692f758b459b459570af759f1ab2478a5e
Dennis Kristiansen <denniskristiansen@protonmail.com>
2022-10-11 17:17:39 +0000

    Migrate to React 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import ReactDOM from "react-dom"
 import React from "react"
 import App from "./App"
+import { createRoot } from "react-dom/client"
 
-ReactDOM.render(
+const container = document.getElementById("root")
+const root = createRoot(container)
+
+root.render(
 	<React.StrictMode>
 		<App />
-	</React.StrictMode>,
-	document.querySelector("#root")
+	</React.StrictMode>
 )
