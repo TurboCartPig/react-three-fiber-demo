d568fd5662a0e153c91a9669a538f4703a3dd962
Dennis Kristiansen <denniskristiansen@protonmail.com>
2022-10-11 17:13:17 +0000

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+
 import React, { Suspense, useReducer, useRef, useState } from "react"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls, useGLTF, useProgress } from "@react-three/drei"
